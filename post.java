2c6404937e0ff9cacb4979f0aef630739c79ad59
assoygo <asylkhankozhanov888@gmail.com>
2025-01-01 21:53:55 +0500

    Create post.java

---
+++ post.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String content;
+    private String author;
+
+    public Post(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", content='" + content + '\'' + ", author='" + author + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
